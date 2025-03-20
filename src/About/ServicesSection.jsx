6f2254cdab4cd762a8a9f4b471f7465cb615a7e8
Mohit <mohitarora2435@gmail.com>
2025-03-20 23:33:10 +0530

    About

--- src/About/ServicesSection.jsx
+++ src/About/ServicesSection.jsx
@@ -15,12 +15,12 @@ const ServicesSection = () => {
         </div>
   
         {/* Image Gallery */}
-        <div className="flex justify-center items-center gap-3 mt-8">
-          <img src="/images/image1.jpg" alt="Service 1" className="w-24 h-24 md:w-32 md:h-32 rounded-lg object-cover shadow-md" />
-          <img src="/images/image2.jpg" alt="Service 2" className="w-32 h-32 md:w-40 md:h-40 rounded-lg object-cover shadow-lg" />
-          <img src="/images/image3.jpg" alt="Service 3" className="w-48 h-48 md:w-56 md:h-56 rounded-lg object-cover shadow-xl" />
-          <img src="/images/image4.jpg" alt="Service 4" className="w-32 h-32 md:w-40 md:h-40 rounded-lg object-cover shadow-lg" />
-          <img src="/images/image5.jpg" alt="Service 5" className="w-24 h-24 md:w-32 md:h-32 rounded-lg object-cover shadow-md" />
+        <div className="flex justify-center items-center gap-0 mt-8">
+          <img src="/images/image1.png" alt="Service 1" className="w-24 h-24 md:w-32 md:h-32 rounded-lg object-cover shadow-md" />
+          <img src="/images/image2.png" alt="Service 2" className="w-32 h-32 md:w-40 md:h-40 rounded-lg object-cover shadow-lg" />
+          <img src="/images/image3.png" alt="Service 3" className="w-48 h-48 md:w-56 md:h-56 rounded-lg object-cover shadow-xl" />
+          <img src="/images/image4.png" alt="Service 4" className="w-32 h-32 md:w-40 md:h-40 rounded-lg object-cover shadow-lg" />
+          <img src="/images/image5.png" alt="Service 5" className="w-24 h-24 md:w-32 md:h-32 rounded-lg object-cover shadow-md" />
         </div>
       </div>
     );
