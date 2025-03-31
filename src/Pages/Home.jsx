b0be0777e17b6cf28d93bad86161577d00be2b38
Mohit <mohitarora2435@gmail.com>
2025-03-31 22:53:07 +0530

    video change

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         muted 
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/Main.mp4" type="video/mp4" />
+        <source src="/Main.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
